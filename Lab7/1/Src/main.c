3178e0a25c905b43b64b2985a6e747644637d6db
amk3y <37068498+amk3y@users.noreply.github.com>
2022-10-24 20:09:28 +0800

    Update main.c

--- Lab7/1/Src/main.c
+++ Lab7/1/Src/main.c
@@ -214,7 +214,9 @@ static void MX_GPIO_Init(void)
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOF_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOB, GPIO_PIN_10|GPIO_PIN_11|GPIO_PIN_12|GPIO_PIN_13 
