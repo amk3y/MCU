4e3bad0aad0b1d136fe839822e10ebf97208ef46
amk3y <37068498+amk3y@users.noreply.github.com>
2022-10-24 18:23:13 +0800

    Enable pull-up resistor

--- Lab7/1/Src/main.c
+++ Lab7/1/Src/main.c
@@ -226,7 +226,7 @@ static void MX_GPIO_Init(void)
   /*Configure GPIO pins : PC13 PC14 PC15 PC12 */
   GPIO_InitStruct.Pin = GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15|GPIO_PIN_12;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
   /*Configure GPIO pins : PB10 PB11 PB12 PB13 
