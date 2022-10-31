a9f4fe4574027244aa22150163063c4ce6aebd26
amk3y <37068498+amk3y@users.noreply.github.com>
2022-10-31 14:59:00 +0800

    Regenerate Lab 8-3

--- Lab8/3/Src/main.c
+++ Lab8/3/Src/main.c
@@ -61,159 +61,6 @@ static void MX_TIM3_Init(void);
 
 /* USER CODE END 0 */
 
-
-int Number[10] = {
-	0xFC00, 0x6000, 0xDA00,
-	0xF200, 0x6600, 0xB600,
-	0xBE00, 0xE000, 0xFE00, 
-	0xE600
-};
-
-enum KeyCode {
-	
-	KEY_0,
-	KEY_1, KEY_2, KEY_3,
-	KEY_4, KEY_5, KEY_6,
-	KEY_7, KEY_8, KEY_9,
-	KEY_A, KEY_B, KEY_C, KEY_D,
-	KEY_ASTERISK, KEY_HASH,
-	KEY_UNDEFINED
-};
-
-int translateKeyCodeToLED(enum KeyCode code){
-	if((int) code < 10)
-		return Number[(int) code];
-	else if(code == KEY_A)
-		return 0xEE00;
-	else if(code == KEY_B)
-		return 0x3E00;
-	else if(code == KEY_C)
-        //0001 1010
-		return 0x1A00;
-	else if(code == KEY_D)
-			// 0111 1010
-		return 0x7A00;
-	else if(code == KEY_ASTERISK)
-		//1001 1110
-		return 0x9E00;
-	else if(code == KEY_HASH)
-		//1000 1110
-		return 0x8E00;
-	else 
-		return 0x9000;
-}
-
-enum KeyCode scanKey(int row){
-	switch (row){
-		case 0: {
-			if(HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_12) == 0) return KEY_1;
-			else if(HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13) == 0) return KEY_2;
-			else if(HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_14) == 0) return KEY_3;
-			else if(HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_15) == 0) return KEY_A;
-			else return KEY_UNDEFINED;
-		}
-		case 1: {
-			if(HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_12) == 0) return KEY_4;
-			else if(HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13) == 0) return KEY_5;
-			else if(HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_14) == 0) return KEY_6;
-			else if(HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_15) == 0) return KEY_B;
-			else return KEY_UNDEFINED;
-		}
-		case 2: {
-			if(HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_12) == 0) return KEY_7;
-			else if(HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13) == 0) return KEY_8;
-			else if(HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_14) == 0) return KEY_9;
-			else if(HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_15) == 0) return KEY_C;
-			else return KEY_UNDEFINED;
-		}
-		case 3: {
-			if(HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_12) == 0) return KEY_ASTERISK;
-			else if(HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13) == 0) return KEY_0;
-			else if(HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_14) == 0) return KEY_HASH;
-			else if(HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_15) == 0) return KEY_D;
-			else return KEY_UNDEFINED;
-		}
-		default: {
-			return KEY_UNDEFINED;
-		}
-	}
-}
-
-
-
-volatile int value = 9995;
-enum KeyCode lastKeyOfRow[4] = {KEY_UNDEFINED};
-enum KeyCode currentKeyOfRow[4] = {KEY_UNDEFINED};
-int64_t keyLastUpdate = 0L; 
-
-	
-volatile int scanningIndex = 0;
-volatile int displayArray[4] = {0,0,0,0};
-
-void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim){
-	if(htim->Instance != TIM3) return;
-	
-	// Reset Scan Lines -> GPIOC 8 ~ 11  
-	HAL_GPIO_WritePin(GPIOC, 0x0F00, GPIO_PIN_SET);
-	// Reset LED Lines 
-	HAL_GPIO_WritePin(GPIOB, 0xFF00, GPIO_PIN_RESET);
-	
-	// Set Current Scan Line (From PC8 to PC11)
-	// 0000 1111 0000 0000
-	HAL_GPIO_WritePin(GPIOC, 0x0100 << scanningIndex, GPIO_PIN_RESET);
-	enum KeyCode keycode = scanKey(scanningIndex);
-	if(keycode != KEY_UNDEFINED){
-		displayArray[0] = translateKeyCodeToLED(keycode);
-	}
-	
-	//Debounce 
-	if(keycode != lastKeyOfRow[scanningIndex]){
-			keyLastUpdate = HAL_GetTick();
-	}
-	
-	if((HAL_GetTick() - keyLastUpdate) > 100){
-				if(keycode != currentKeyOfRow[scanningIndex]){
-					currentKeyOfRow[scanningIndex] = keycode;
-					if(keycode != KEY_UNDEFINED){
-						// Button Action
-						 for (int i = 4; i > 0; i--) {
-							  if(i == 4) continue;
-							  else displayArray[i] = displayArray[i - 1];
-							}
-						// Button Action End
-					}
-				}
-			}
-	
-			lastKeyOfRow[scanningIndex] = keycode;
-			//Debounce End
-			
-			//
-			// Do Display
-			//
-			
-
-			//
-			// Do Display
-			//
-		HAL_GPIO_WritePin(GPIOB, 0xFF00, GPIO_PIN_RESET);
-		HAL_GPIO_WritePin(GPIOB, displayArray[scanningIndex], GPIO_PIN_SET);
-		
-	
-		scanningIndex++;
-		if(scanningIndex >= 4) scanningIndex = 0;
-
-}
-//#ifdef __cplusplus
-//}
-//#endif
-
-
-
-
-
-
-
 /**
   * @brief  The application entry point.
   * @retval int
@@ -317,7 +164,7 @@ static void MX_TIM3_Init(void)
   htim3.Instance = TIM3;
   htim3.Init.Prescaler = 47999;
   htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim3.Init.Period = 1;
+  htim3.Init.Period = 999;
   htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_Base_Init(&htim3) != HAL_OK)
@@ -355,8 +202,9 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_10|GPIO_PIN_11|GPIO_PIN_12|GPIO_PIN_13 
-                          |GPIO_PIN_14|GPIO_PIN_15|GPIO_PIN_8|GPIO_PIN_9, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_2|GPIO_PIN_10|GPIO_PIN_11|GPIO_PIN_12 
+                          |GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15|GPIO_PIN_8 
+                          |GPIO_PIN_9, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8|GPIO_PIN_9|GPIO_PIN_10|GPIO_PIN_11, GPIO_PIN_RESET);
@@ -367,16 +215,12 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : PB2 PB3 */
-  GPIO_InitStruct.Pin = GPIO_PIN_2|GPIO_PIN_3;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-
-  /*Configure GPIO pins : PB10 PB11 PB12 PB13 
-                           PB14 PB15 PB8 PB9 */
-  GPIO_InitStruct.Pin = GPIO_PIN_10|GPIO_PIN_11|GPIO_PIN_12|GPIO_PIN_13 
-                          |GPIO_PIN_14|GPIO_PIN_15|GPIO_PIN_8|GPIO_PIN_9;
+  /*Configure GPIO pins : PB2 PB10 PB11 PB12 
+                           PB13 PB14 PB15 PB8 
+                           PB9 */
+  GPIO_InitStruct.Pin = GPIO_PIN_2|GPIO_PIN_10|GPIO_PIN_11|GPIO_PIN_12 
+                          |GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15|GPIO_PIN_8 
+                          |GPIO_PIN_9;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
@@ -388,10 +232,6 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
-
-  /* EXTI interrupt init*/
-  HAL_NVIC_SetPriority(EXTI2_3_IRQn, 0, 0);
-  HAL_NVIC_EnableIRQ(EXTI2_3_IRQn);
 
 }
 
